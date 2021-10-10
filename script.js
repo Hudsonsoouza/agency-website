39a4fe4f009ac8da5df714dacdf314c8a006353b
Hudson Souza (Own3d 私) <70814948+Hudsonsoouza@users.noreply.github.com>
2021-10-10 04:00:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// JQUERY SLICK SLIDER
+
+$('.container-slider').slick({
+    dots: true,
+    arrows:false,
+    speed:1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover:false,
+    responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }],
+    });
+
+    // MOSTRAR E OCULTAR MENU MOBILE
+
+    var menuBtn = document.querySelector('.items-menu-mobile i');
+    menuBtn.addEventListener('click',()=>{
+        let itemsMenu = document.querySelector('.menu-mobile');
+        if(itemsMenu.classList.contains('show')){
+            itemsMenu.classList.remove('show');
+            itemsMenu.classList.add('hide');
+        }else{
+            itemsMenu.classList.remove('hide');
+            itemsMenu.classList.add('show');
+        };
+    })
